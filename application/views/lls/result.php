d6f8ec999dbe8710d5fa9dd06a2c26fb48581bc2
YukiMiura <miura@alfasystem.co.jp>
2017-08-22 14:35:27 +0900

    リネームに伴う変更

--- application/views/lls/result.php
+++ application/views/lls/result.php
@@ -18,7 +18,7 @@
     </table>
     <h2><?php echo $result;?></h2>
     <?php if($dialog){?>
-      <?php echo form_open('LunarLanderServer/Inherit');?>
+      <?php echo form_open('LLS/Inherit');?>
       <button name="YesNo" value="0">No</button>　　<button name = "YesNo" value="1">Yes</button>
     </form>
     <?php } ?>
